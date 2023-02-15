3b67f10fef196f6749106a21596199c00926baac
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-02-16 00:32:27 +0300

    Add files via upload

---
+++ JavaHW6/Catalog.java
@@ -0,0 +1,9 @@
+package JavaHW6;
+
+public class Catalog extends Laptop {
+
+    public Catalog(String name, int ram, int ssd, String os, String color){
+        super(name,ram,ssd,os,color);
+    }
+ 
+}
