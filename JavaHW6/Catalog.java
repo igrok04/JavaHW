933f23d2383a85bea550726be10f76aee7365311
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-02-16 00:33:42 +0300

    Delete JavaHW6 directory

--- JavaHW6/Catalog.java
+++
@@ -1,9 +0,0 @@
-package JavaHW6;
-
-public class Catalog extends Laptop {
-
-    public Catalog(String name, int ram, int ssd, String os, String color){
-        super(name,ram,ssd,os,color);
-    }
- 
-}
